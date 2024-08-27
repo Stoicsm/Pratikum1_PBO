3d7ef131e065aa1b37416f6ed90ee97b9b1e0b6a
Jonathan Aditya Saputra <161813325+Stoicsm@users.noreply.github.com>
2024-08-27 17:45:05 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t1;
+
+/**
+ *
+ * @author BLACK
+ */
+public class Mahasiswa {
+         void membaca(){
+        System.out.println("sedang membaca");
+    }
+    
+    void menyontek(){
+        System.out.println("mencontek");
+    }
+    
+    void memodifikasi(){
+        System.out.println("memodifikasi hasil contekan");
+    }
+}
---
+++ MahasiswaBeraksi.java
@@ -0,0 +1,20 @@
+package t1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author BLACK
+ */
+public class MahasiswaBeraksi {
+    public static void main(String[] args){
+    Mahasiswa mhs = new Mahasiswa();
+    
+    mhs.menyontek();
+    mhs.membaca();
+    mhs.memodifikasi();
+    }
+}
